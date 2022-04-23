11710046749063b3352756beb1864988b1f508b3
Thiago Gomes de Oliveira <infogomes@gmail.com>
2022-04-23 19:09:20 +0100

    Save transactions on the database implemented

--- analysisfinancial/src/main/java/br/com/infogomes/analysisfinancial/controller/FileController.java
+++ analysisfinancial/src/main/java/br/com/infogomes/analysisfinancial/controller/FileController.java
@@ -2,6 +2,7 @@ package br.com.infogomes.analysisfinancial.controller;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,10 +10,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.com.infogomes.analysisfinancial.services.TransactionService;
 import br.com.infogomes.analysisfinancial.util.CSVUtil;
 
 @Controller
 public class FileController {
+
+	@Autowired
+	private TransactionService service;
 
 	@GetMapping("/")
 	public String helloWorld() {
@@ -20,10 +25,16 @@ public class FileController {
 	}
 
 	@PostMapping("/")
-	public String handleFileUpload(@RequestParam("formFile") MultipartFile file,
-			RedirectAttributes redirectAttributes) throws IOException {
+	public String handleFileUpload(@RequestParam("formFile") MultipartFile file, RedirectAttributes redirectAttributes)
+			throws IOException {
+		
+		if(CSVUtil.isEmpty(file.getInputStream())) {			
+			throw new RuntimeException("Aquivo Vazio");
+		}
 
 		CSVUtil.readeCsv(file.getInputStream());
+
+		service.save(CSVUtil.parseToTransactionList(file.getInputStream()));
 
 		System.out.println("Nome do arquivo: " + file.getOriginalFilename() + " tamanho do arquivo: " + file.getSize());
 
--- analysisfinancial/src/main/java/br/com/infogomes/analysisfinancial/util/CSVUtil.java
+++ analysisfinancial/src/main/java/br/com/infogomes/analysisfinancial/util/CSVUtil.java
@@ -3,12 +3,21 @@ package br.com.infogomes.analysisfinancial.util;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import com.opencsv.exceptions.CsvValidationException;
 
-public class CSVUtil {
+import br.com.infogomes.analysisfinancial.entities.Transaction;
+
+public abstract class CSVUtil {
+
 	public static void readeCsv(InputStream inputStream) {
 		try (CSVReader csvReader = new CSVReader(new InputStreamReader(inputStream))) {
 
@@ -20,5 +29,54 @@ public class CSVUtil {
 		} catch (IOException | CsvValidationException e) {
 			// TODO: handle exception
 		}
+	}
+
+	public static List<String[]> parseToTransactionList(InputStream inputStream) {
+
+		List<String[]> transactions = new ArrayList<>();
+
+		try (CSVReader csvReader = new CSVReader(new InputStreamReader(inputStream))) {
+
+			String[] transactionLine;
+
+			while ((transactionLine = csvReader.readNext()) != null) {
+				transactions.add(transactionLine);
+			}
+
+		} catch (IOException | CsvValidationException e) {
+			// TODO: handle exception
+		}
+
+		return transactions;
+	}
+
+	public static Transaction parseToTransaction(String[] fields) {
+		String sourceBank = fields[0];
+		String sourceAgency = fields[1];
+		String sourceAccount = fields[2];
+		String destinationBank = fields[3];
+		String destinationAgency = fields[4];
+		String destinationAccount = fields[5];
+
+		BigDecimal value = BigDecimal.valueOf(Double.valueOf(fields[6]));
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+		LocalDateTime dateTime = LocalDateTime.parse(fields[7], formatter);
+
+		return new Transaction(sourceBank, sourceAgency, sourceAccount, destinationBank, destinationAgency,
+				destinationAccount, value, dateTime);
+	}
+
+	public static boolean isEmpty(InputStream inputStream) {
+
+		try (CSVReader csvReader = new CSVReader(new InputStreamReader(inputStream))) {
+			
+			return csvReader.readAll().size() <= 0;
+
+		} catch (IOException | CsvException e) {
+			// TODO: handle exception
+		}
+
+		return true;
 	}
 }
