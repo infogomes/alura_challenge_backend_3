d1afb62a79b8c1cfb046c5d8571f029f7c211706
Thiago Gomes de Oliveira <infogomes@gmail.com>
2022-04-23 12:42:53 +0100

    Read CSV file to console

--- analysisfinancial/src/main/java/br/com/infogomes/analysisfinancial/controller/FileController.java
+++ analysisfinancial/src/main/java/br/com/infogomes/analysisfinancial/controller/FileController.java
@@ -1,11 +1,15 @@
 package br.com.infogomes.analysisfinancial.controller;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.infogomes.analysisfinancial.util.CSVUtil;
 
 @Controller
 public class FileController {
@@ -16,9 +20,12 @@ public class FileController {
 	}
 
 	@PostMapping("/")
-	public String handleFileUpload(@RequestParam("formFile") MultipartFile file, RedirectAttributes redirectAttributes) {
+	public String handleFileUpload(@RequestParam("formFile") MultipartFile file,
+			RedirectAttributes redirectAttributes) throws IOException {
 
-		System.out.println("Nome do arquivo: "+file.getOriginalFilename() + " tamanho do arquivo: "+ file.getSize());
+		CSVUtil.readeCsv(file.getInputStream());
+
+		System.out.println("Nome do arquivo: " + file.getOriginalFilename() + " tamanho do arquivo: " + file.getSize());
 
 		return "redirect:/";
 	}
